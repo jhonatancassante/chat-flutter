1e15af560fbee389159d97e33e9742c1adab4d57
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2024-07-29 20:34:40 -0300

    fix: fixed image upload to firebase

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -15,6 +15,7 @@ class _AuthPageState extends State<AuthPage> {
 
   Future<void> _handleSubmit(AuthFormData formData) async {
     try {
+      if (!mounted) return;
       setState(() => _isLoading = true);
 
       if (formData.isLogin) {
@@ -33,7 +34,9 @@ class _AuthPageState extends State<AuthPage> {
     } catch (error) {
       //Tratar erro!
     } finally {
-      setState(() => _isLoading = false);
+      if (mounted) {
+        setState(() => _isLoading = false);
+      }
     }
   }
 
